c090fd3ed771f69e4825d518f2d17dda7c439621
OHzz <brb1111@naver.com>
2019-08-04 18:13:31 +0900

    Polls app FBV -> CBV

--- polls/views.py
+++ polls/views.py
@@ -1,25 +1,39 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 from polls.models import Choice, Question
+
 # from .forms import NameForm
 
-def index(request):
-    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(
-        request,
-        'polls/index.html',
-        context
-    )
+# def index(request):
+#     latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(
+#         request,
+#         'polls/index.html',
+#         context
+#     )
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id) #Question 모델클래스로부터 pk=question_id 검색 조건에 맞는 객체를 조회합니다.
-    return render(
-        request,
-        'polls/detail.html',
-        {'question': question},
-    )
+    def get_queryset(self):
+        return Question.objects.order_by('pub_date')[:5]
+
+
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id) #Question 모델클래스로부터 pk=question_id 검색 조건에 맞는 객체를 조회합니다.
+#     return render(
+#         request,
+#         'polls/detail.html',
+#         {'question': question},
+#     )
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
 def vote(request, question_id):
@@ -37,13 +51,18 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(
-        request,
-        'polls/results.html',
-        {'question': question},
-    )
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(
+#         request,
+#         'polls/results.html',
+#         {'question': question},
+#     )
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 # def get_name(request):
 #     if request.method == 'POST':
